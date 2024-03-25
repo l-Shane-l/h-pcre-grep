bd114aa130a31e166e8afa2f952e38644416200a
l-Shane-l <graainneog@proton.me>
2024-03-25 09:11:55 +0100

    1st stage

--- app/Main.hs
+++ app/Main.hs
@@ -2,27 +2,27 @@ module Main where
 
 import System.Environment
 import System.Exit
+import Data.Char (isDigit)
 
+-- Updated to handle single characters and the \d pattern for digits
 matchPattern :: String -> String -> Bool
-matchPattern pattern input = do
-  if length pattern == 1
-    then head pattern `elem` input
-    else error $ "Unhandled pattern: " ++ pattern
+matchPattern pattern input =
+  case pattern of
+    "\\d" -> any isDigit input   -- Checks for any digit in the input
+    [c]   -> c `elem` input      -- Checks if the single character is in the input
+    _     -> error $ "Unhandled pattern: " ++ pattern
 
 main :: IO ()
 main = do
   args <- getArgs
-  let pattern = args !! 1
-  input_line <- getLine
+  if head args /= "-E" || length args < 2
+    then do
+      putStrLn "Expected first argument to be '-E' and a pattern provided"
+      exitFailure
+    else do
+      let pattern = args !! 1
+      input_line <- getLine
+      if matchPattern pattern input_line
+        then exitSuccess
+        else exitFailure
 
-  -- You can use print statements as follows for debugging, they'll be visible when running tests.
-  putStrLn "Logs from your program will appear here"
-
-  -- Uncomment this block to pass stage 1
-  -- if head args /= "-E"
-  --   then do
-  --     putStrLn "Expected first argument to be '-E'"
-  --     exitFailure
-  --   else do if matchPattern pattern input_line
-  --             then exitSuccess
-  --             else exitFailure
